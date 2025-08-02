233d60d42269303fadc72d302a279700c0352bce
Kunal Telangi <74617472+kunaltelangi@users.noreply.github.com>
2025-08-02 21:42:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import App from './App.jsx'
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       suspense: true,
-    },
-  },
-});
+      retry: 1,
+      staleTime: 1000 * 60 * 5,   // 5 minutes
+      cacheTime: 1000 * 60 * 30   // 30 minutes
+    }
+  }
+})
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={ queryClient }>
-      <ReactQueryDevtools />
+    <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   </React.StrictMode>
-);
+)
