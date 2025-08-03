72ec0e60b6aeea8435011836fd6d7f3906f24979
Kunal Telangi <74617472+kunaltelangi@users.noreply.github.com>
2025-08-03 19:20:25 +0530

    Update PokemonCard.jsx

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,39 +1,69 @@
-import React from 'react';
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
 import { usePokemonModal } from '../context/PokemonModalProvider';
 import { getTypeIconSrc } from '../utils/pokemon-helper';
+import './PokemonCard.css';
 
-const PokemonCard = ({ pokemon, pokemon: { paddedId, name, types, imgSrc } }) => {
-    const { openModal } = usePokemonModal();
+const PokemonCard = ({ pokemon }) => {
+  const { id, paddedId, name, types, imgSrc } = pokemon;
+  const { openModal } = usePokemonModal();
 
-    return (
-        <div
-            onClick={ () => openModal(pokemon) }
-            className={ `pokemon-card ${ types[0].name }` }
-        >
-            <div>
-                <span className='id-number'>{ '#' + paddedId }</span>
-                <span className='pokemon-name'>{ name }</span>
+  const handleClick = () => openModal(pokemon);
 
-                <div className='types'>
-                    {
-                        types.map(({ name }) => {
-                            const typeImg = getTypeIconSrc(name);
-                            
-                            return (
-                                <div key={ name } className={ name }>
-                                    <img src={ typeImg } alt={ name } />
-                                    <span className='type-name'>{ name }</span>
-                                </div>
-                            );
-                        })
-                    }
-                </div>
-            </div>
+  return (
+    <button
+      type="button"
+      onClick={handleClick}
+      className={`pokemon-card ${types[0]?.name}`}
+      aria-label={`View details for ${name}`}
+    >
+      <div className="pokemon-card__info">
+        <span className="pokemon-card__id">#{paddedId}</span>
+        <span className="pokemon-card__name">{name}</span>
 
-            <div className='pokeball-bg'></div>
-            <img className='pokemon-image' src={ imgSrc } alt='pokemon-image' />
+        <div className="pokemon-card__types">
+          {types.map(({ name: typeName }) => {
+            const typeImg = getTypeIconSrc(typeName);
+            return (
+              <div
+                key={typeName}
+                className={`pokemon-card__type pokemon-card__type--${typeName}`}
+              >
+                <img
+                  src={typeImg}
+                  alt={`${typeName} icon`}
+                  className="pokemon-card__type-icon"
+                  loading="lazy"
+                />
+                <span className="pokemon-card__type-name">{typeName}</span>
+              </div>
+            );
+          })}
         </div>
-    );
+      </div>
+
+      <div className="pokemon-card__background" aria-hidden="true" />
+      <img
+        className="pokemon-card__image"
+        src={imgSrc}
+        alt={name}
+        loading="lazy"
+        onError={(e) => { e.target.onerror = null; e.target.src = '/images/placeholder.png'; }}
+      />
+    </button>
+  );
 };
 
-export default PokemonCard;
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    paddedId: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    types: PropTypes.arrayOf(
+      PropTypes.shape({ name: PropTypes.string.isRequired })
+    ).isRequired,
+    imgSrc: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default memo(PokemonCard);
